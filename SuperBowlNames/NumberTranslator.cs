e2ffc36ff9f1ce17e329f08349f3b9fc6d062409
Caseywalker <casey.walker118@gmail.com>
2021-09-27 19:15:56 -0500

    Adding unit testing for super bowl names

--- SuperBowlNames/NumberTranslator.cs
+++ SuperBowlNames/NumberTranslator.cs
@@ -11,13 +11,38 @@ namespace SuperBowlNames
     public string Translate(int numberToTranslate)
     {
       //switch expression
-      return numberToTranslate switch
-      {
-        1 => "I",
-        3 => "III",
-        4 => "IV",
-        _ => ""
-      };
+      //return numberToTranslate switch
+      //{
+      //  1 => "I",
+      //  3 => "III",
+      //  4 => "IV",
+      //  5 => "v",
+      //  _ => ""
+      //};
+
+      var numeral = "";
+
+      numeral += new string('L', numberToTranslate / 50);
+      numberToTranslate %= 50;
+
+      numeral += numberToTranslate >= 40 ? "XL" : "";
+      numberToTranslate %= 40;
+
+      numeral += new string('X', numberToTranslate / 10);
+      numberToTranslate %= 10;
+
+      numeral += numberToTranslate == 9 ? "IX" : "";
+      numberToTranslate %= 9;
+
+      numeral += new string('V', numberToTranslate / 5);
+      numberToTranslate %= 5;
+
+      numeral += numberToTranslate == 4 ? "IV" : "";
+      numberToTranslate %= 4;
+
+      numeral += new string('I', numberToTranslate);
+
+      return numeral;
 
 
       //switch (numberToTranslate)
